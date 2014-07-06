b5208bc1e089d77e93c1397e8752417b6986650d
Marjori Pomarole <marjoripomarole@gmail.com>
2014-07-06 16:19:08 -0300

    Done with part 2 and 3w

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,28 @@
 import sys
-
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
+import json
 
 def main():
-    sent_file = open(sys.argv[1])
+    afinnfile = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    scores = {}
+    for line in afinnfile:
+        term, score  = line.split("\t")
+        scores[term] = int(score)
+
+    for line in tweet_file:
+        tweet = json.loads(line)
+        tweet_text = tweet.get('text', None)
+        if tweet_text:
+            tweet_score = 0
+            not_found = []
+            for word in tweet_text.split(" "):
+                word_score = scores.get(word, 0)
+                tweet_score =+ word_score
+                if word_score == 0:
+                    not_found.append(word)
+            for word in not_found:
+                print "%s %d" % (word, tweet_score)
 
 if __name__ == '__main__':
     main()
